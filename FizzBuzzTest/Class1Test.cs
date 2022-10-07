c2f2da4ddae710e37bee19a2df019b9069ff0cdc
Wu Xueheng <xhwu@thougtworks.com>
2022-10-08 07:06:19 +0800

    first commit

---
+++ FizzBuzzTest/Class1Test.cs
@@ -0,0 +1,18 @@
+using FizzBuzz;
+using Moq;
+using Xunit;
+
+namespace FizzBuzzTest
+{
+    public class Class1Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            Class1 class1 = new Class1();
+
+            //then
+            Assert.NotNull(class1);
+        }
+    }
+}
