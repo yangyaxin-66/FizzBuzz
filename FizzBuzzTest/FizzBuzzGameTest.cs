dd69255ffd22fe2ddf6165b469516eb42403c96c
Yaxin Yang <yyang66@slb.com>
2023-11-01 14:54:14 +0800

    test: creat all test

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -16,5 +16,41 @@ namespace FizzBuzzTest
             //then
             Assert.Equal("1", result);
         }
+
+        [Fact]
+        public void Should_return_normalNumber_when_countof_given_3mutipleNumber()
+        {
+            //given
+            int givenNumber = 3;
+            //when
+            string result = FizzBuzzGame.CountOff(givenNumber);
+
+            //then
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void Should_return_normalNumber_when_countof_given_5mutipleNumber()
+        {
+            //given
+            int givenNumber = 5;
+            //when
+            string result = FizzBuzzGame.CountOff(givenNumber);
+
+            //then
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void Should_return_normalNumber_when_countof_given_3and5mutipleNumber()
+        {
+            //given
+            int givenNumber = 15;
+            //when
+            string result = FizzBuzzGame.CountOff(givenNumber);
+
+            //then
+            Assert.Equal("FizzBuzz", result);
+        }
     }
 }
